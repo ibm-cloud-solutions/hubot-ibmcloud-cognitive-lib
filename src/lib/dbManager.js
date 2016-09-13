89c97997c53c69710c0886e8f25b2759d1048903
Jorge Padilla <jpadilla@us.ibm.com>
2016-09-12 20:45:38 -0400

    Revert breaking changes

--- src/lib/dbManager.js
+++ src/lib/dbManager.js
@@ -13,6 +13,8 @@ const PouchDB = require('./PouchDB');
 
 const pjson = require(path.resolve(process.cwd(), 'package.json'));
 const classesDesignDoc = '_design/classes';
+const classesView = 'classes/byClass';
+const targetView = 'classes/byTarget';
 
 let managedDBs = {};
 /**
@@ -237,5 +239,122 @@ DBManager.prototype.info = function(opts){
 	});
 };
 
+
+/**
+ * @deprecated Use nlcconfig.getAllClasses.
+ */
+DBManager.prototype.getAllClasses = function(approvedAfterDate) {
+	// assumption that the database can be held in memory
+	// note classifier will break if this is not the case
+	// return an array of [text, className]
+	return new Promise((resolve, reject) => {
+		let result = [];
+		if (this.db){
+			// get all of the class types
+			return this.db.query(classesView, {
+				include_docs: true
+			}).then((res) => {
+				if (approvedAfterDate && typeof approvedAfterDate === 'number'){
+					approvedAfterDate = new Date(approvedAfterDate);
+				}
+
+				for (let row of res.rows){
+					// Filter records without an approvedDate or approved before the given date.
+					if (approvedAfterDate) {
+						if (row.doc.approved){
+							let approvedDate = row.doc.approved_timestamp || row.doc.approved;
+							if (new Date(parseInt(approvedDate, 10)) < approvedAfterDate){
+								continue;
+							}
+						}
+						else {
+							continue;
+						}
+					}
+
+					let className = row.key;
+					// allow short hand assignment for classifications
+					let text = row.doc.text || row.doc.classification.text;
+
+					result.push([
+						text, className
+					]);
+				}
+				return resolve(result);
+			}).catch(function(err) {
+				reject(err);
+			});
+		}
+		else {
+			reject('Database needs to be open before calling getLocalClasses');
+		}
+	});
+};
+
+/**
+ * @deprecated Use nlcconfig.getClassEmitTarget.
+ */
+DBManager.prototype.getClassEmitTarget = function(className) {
+	return this.db.query(targetView, {
+		key: className
+	}).then((result) => {
+		if (result.rows.length > 0){
+			let resp = {
+				class: result.rows[0].id,
+				description: result.rows[0].value.length >= 3 ? result.rows[0].value[2] : result.rows[0].id,
+				target: result.rows[0].value[0],
+				parameters: result.rows[0].value[1]
+			};
+
+			// loop through and resolve $ref if defined
+			if (resp.parameters){
+				let ps = [];
+				for (let p of resp.parameters){
+					ps.push(
+						new Promise((resolve, reject) => {
+							if (p.values && !Array.isArray(p.values)){
+								if (p.values.$ref){
+									let ref = p.values.$ref;
+									return this.db.get(ref).then((doc) => {
+										if (doc.values){
+											p.values = doc.values;
+											resolve(p);
+										}
+										else {
+											resolve(p);
+										}
+									});
+								}
+								else {
+									// skip over, return object
+									resolve(p);
+								}
+							}
+							else {
+								resolve(p);
+							}
+						})
+					);
+				}
+				return Promise.all(ps).then((params) => {
+					resp.parameters = params;
+					return resp;
+				}).catch(() => {
+					// can't execute this emit target, so return null
+					logger.error(`${TAG} Couldn't resolve parameters for class ${className}. This is likely caused by incorrect data in the database. Was trying to resolve initial params`, resp.parameters);
+					return null;
+				});
+			}
+			else {
+				// no parameters
+				return resp;
+			}
+		}
+		else {
+			logger.error(`${TAG} Class ${className} doesn't exist in the database. This is likely an indication of (1) the Watson NLC needs to be re-trained with the current data, or (2) a problem initializing or synchronizing the database.`);
+			return null;
+		}
+	});
+};
 
 module.exports = DBManager;
