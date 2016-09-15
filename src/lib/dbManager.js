dfee9b6d7364d43eca8b08990e7af5f02af73c49
Ramakrishna Boggarapu <ramab@us.ibm.com>
2016-09-15 16:57:45 -0400

    use localDbPath from options (#98)
    * use localDbPath from options
    
    * Add additional log statement
    
    * update the default local db path

--- src/lib/dbManager.js
+++ src/lib/dbManager.js
@@ -37,6 +37,7 @@ function DBManager(options){
 
 	this.localDbName = (typeof options === 'string') ? options : options.localDbName;
 	this.remoteDbName = options.remoteDbName ? options.remoteDbName : this.localDbName;
+	this.localDbPath = options.localDbPath ? options.localDbPath : env.dbPath;
 	this.db = PouchDB.open(this.localDbName, this.localDbPath);
 
 	initializeDB(this.db).then(() => {
